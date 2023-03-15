fe67acbb3da88fc679c017688f3a149a8114e8f3
Tristan <94827589+chiwu7@users.noreply.github.com>
2023-03-15 11:17:18 +0100

    Add test trainWithTwoMails

--- src/test/TestClassifier.java
+++ src/test/TestClassifier.java
@@ -48,4 +48,31 @@ class TestClassifier {
         assertEquals(0.0, classifier.getHamProbability(), 0.001);
     }
 
+	@Test
+    public void trainWithTwoMails() {
+		NaiveBayesClassifier classifier = new NaiveBayesClassifier();
+		List<Mail> trainingData = Arrays.asList(
+				new Mail("This is a spam email", true),
+				new Mail("This is a ham email", false)
+				);
+		classifier.train(trainingData);
+		Map<String, Double> expectedSpamWordCounts = new HashMap<>();
+		expectedSpamWordCounts.put("This", 2.0);
+		expectedSpamWordCounts.put("is", 2.0);
+		expectedSpamWordCounts.put("a", 2.0);
+		expectedSpamWordCounts.put("spam", 2.0);
+		expectedSpamWordCounts.put("email", 2.0);
+		assertEquals(expectedSpamWordCounts, classifier.getSpamWordsProb());
+
+		Map<String, Double> expectedHamWordCounts = new HashMap<>();
+		expectedHamWordCounts.put("This", 2.0);
+		expectedHamWordCounts.put("is", 2.0);
+		expectedHamWordCounts.put("a", 2.0);
+		expectedHamWordCounts.put("ham", 2.0);
+		expectedHamWordCounts.put("email", 2.0);
+		assertEquals(expectedHamWordCounts, classifier.getHamWordsProb());
+
+		assertEquals(0.5, classifier.getSpamProbability(), 0.001);
+		assertEquals(0.5, classifier.getHamProbability(), 0.001);
+    }
 }
