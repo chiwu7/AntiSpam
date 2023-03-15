300a4f4cf16bff5018007b7399e11d13582e8248
Tristan <94827589+chiwu7@users.noreply.github.com>
2023-03-15 11:19:34 +0100

    Add test testPredictWithOneMail

--- src/test/TestClassifier.java
+++ src/test/TestClassifier.java
@@ -84,6 +84,16 @@ class TestClassifier {
 		String result = classifier.predict(testData);
 		assertEquals("LaPlace : The number of spam mail is : 0\nLaPlace : The number of ham mail is : 0", result);
 	}
-	
+
+	@Test
+	public void testPredictWithOneMail() {
+		NaiveBayesClassifier classifier = new NaiveBayesClassifier();
+		List<Mail> testData = Arrays.asList(
+				new Mail("This is a spam email", true)
+				);
+		classifier.train(testData);
+		String result = classifier.predict(testData);
+		assertEquals("LaPlace : The number of spam mail is : 1\nLaPlace : The number of ham mail is : 0", result);
+	}
 	
 }
