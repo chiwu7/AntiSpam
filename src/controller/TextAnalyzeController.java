2fc1acbd4b5a59ba781aa242472150960ad34156
Déwi <dewi.girot@gmail.com>
2023-03-15 11:01:51 +0100

    Update view and add variables into controller

--- src/controller/TextAnalyzeController.java
+++ src/controller/TextAnalyzeController.java
@@ -3,6 +3,8 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import model.FileChooserSample;
 
@@ -12,6 +14,18 @@ public class TextAnalyzeController {
 	// BorderPane correspondant à la fenetre
 	@FXML
 	private BorderPane root;
+	
+	// Bouton pour chercher les fichiers à analyser dans l'ordinateur de l'utilisateur
+	@FXML
+	private Button chooseFileButton;
+	
+	// Bouton pour vérifier que l'utilisateur a bien importés des fichiers avant de lancer un analyse de spam
+	@FXML
+	private Button checkFilesImported;
+	
+	// Texte en fin de page pour annoncer le nombre de spam et de non spam trouvés
+	@FXML
+	private Label successMessage;
 	
 	
 	public void openFileChooser() {
