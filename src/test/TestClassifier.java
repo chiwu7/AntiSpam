de148d9278898ab2b3ade37d00f4c209113d7d2e
Tristan <94827589+chiwu7@users.noreply.github.com>
2023-03-15 11:21:52 +0100

    Add test testPredictNoMail

--- src/test/TestClassifier.java
+++ src/test/TestClassifier.java
@@ -16,41 +16,41 @@ class TestClassifier {
 
 	@Test
 	public void trainWithZeroMail() {
-	    NaiveBayesClassifier classifier = new NaiveBayesClassifier();
-	    List<Mail> trainingData = Collections.emptyList();
-	    classifier.train(trainingData);
-	    Map<String, Double> expectedSpamWordCounts = new HashMap<>();
-	    Map<String, Double> expectedHamWordCounts = new HashMap<>();
-	    assertEquals(expectedSpamWordCounts, classifier.getSpamWordsProb());
-	    assertEquals(expectedHamWordCounts, classifier.getHamWordsProb());
-	    assertEquals(0.0, classifier.getSpamProbability(), 0.001);
-	    assertEquals(0.0, classifier.getHamProbability(), 0.001);
-	}
-	
-	@Test
-    public void trainWithOneMail() {
 		NaiveBayesClassifier classifier = new NaiveBayesClassifier();
-        List<Mail> trainingData = Arrays.asList(
-                new Mail("This is a spam email", true)
-        );
-        classifier.train(trainingData);
-        Map<String, Double> expectedSpamWordCounts = new HashMap<>();
-        expectedSpamWordCounts.put("This", 2.0);
-        expectedSpamWordCounts.put("is", 2.0);
-        expectedSpamWordCounts.put("a", 2.0);
-        expectedSpamWordCounts.put("spam", 2.0);
-        expectedSpamWordCounts.put("email", 2.0);
-        assertEquals(expectedSpamWordCounts, classifier.getSpamWordsProb());
-
-        Map<String, Double> expectedHamWordCounts = new HashMap<>();
-        assertEquals(expectedHamWordCounts, classifier.getHamWordsProb());
-
-        assertEquals(1.0, classifier.getSpamProbability(), 0.001);
-        assertEquals(0.0, classifier.getHamProbability(), 0.001);
-    }
+		List<Mail> trainingData = Collections.emptyList();
+		classifier.train(trainingData);
+		Map<String, Double> expectedSpamWordCounts = new HashMap<>();
+		Map<String, Double> expectedHamWordCounts = new HashMap<>();
+		assertEquals(expectedSpamWordCounts, classifier.getSpamWordsProb());
+		assertEquals(expectedHamWordCounts, classifier.getHamWordsProb());
+		assertEquals(0.0, classifier.getSpamProbability(), 0.001);
+		assertEquals(0.0, classifier.getHamProbability(), 0.001);
+	}
 
 	@Test
-    public void trainWithTwoMails() {
+	public void trainWithOneMail() {
+		NaiveBayesClassifier classifier = new NaiveBayesClassifier();
+		List<Mail> trainingData = Arrays.asList(
+				new Mail("This is a spam email", true)
+				);
+		classifier.train(trainingData);
+		Map<String, Double> expectedSpamWordCounts = new HashMap<>();
+		expectedSpamWordCounts.put("This", 2.0);
+		expectedSpamWordCounts.put("is", 2.0);
+		expectedSpamWordCounts.put("a", 2.0);
+		expectedSpamWordCounts.put("spam", 2.0);
+		expectedSpamWordCounts.put("email", 2.0);
+		assertEquals(expectedSpamWordCounts, classifier.getSpamWordsProb());
+
+		Map<String, Double> expectedHamWordCounts = new HashMap<>();
+		assertEquals(expectedHamWordCounts, classifier.getHamWordsProb());
+
+		assertEquals(1.0, classifier.getSpamProbability(), 0.001);
+		assertEquals(0.0, classifier.getHamProbability(), 0.001);
+	}
+
+	@Test
+	public void trainWithTwoMails() {
 		NaiveBayesClassifier classifier = new NaiveBayesClassifier();
 		List<Mail> trainingData = Arrays.asList(
 				new Mail("This is a spam email", true),
@@ -75,8 +75,8 @@ class TestClassifier {
 
 		assertEquals(0.5, classifier.getSpamProbability(), 0.001);
 		assertEquals(0.5, classifier.getHamProbability(), 0.001);
-    }
-	
+	}
+
 	@Test
 	public void testPredictWithEmptyList() {
 		NaiveBayesClassifier classifier = new NaiveBayesClassifier();
@@ -95,7 +95,7 @@ class TestClassifier {
 		String result = classifier.predict(testData);
 		assertEquals("LaPlace : The number of spam mail is : 1\nLaPlace : The number of ham mail is : 0", result);
 	}
-	
+
 	@Test
 	public void testPredictWithTwoMails() {
 		NaiveBayesClassifier classifier = new NaiveBayesClassifier();
@@ -107,7 +107,7 @@ class TestClassifier {
 		String result = classifier.predict(testData);
 		assertEquals("LaPlace : The number of spam mail is : 1\nLaPlace : The number of ham mail is : 1", result);
 	}
-	
+
 	@Test
 	public void testPredictOneSpamMail() {
 		NaiveBayesClassifier classifier = new NaiveBayesClassifier();
@@ -119,18 +119,30 @@ class TestClassifier {
 		String result = classifier.predict(newMail);
 		assertEquals("LaPlace : spam", result);
 	}
+
+	@Test
+	public void testPredictOneHamMail() {
+		NaiveBayesClassifier classifier = new NaiveBayesClassifier();
+		List<Mail> trainingData = Arrays.asList(
+				new Mail("This is a spam email", true),
+				new Mail("This is not a spam email", false)
+				);
+		classifier.train(trainingData);
+		Mail newMail = new Mail("This is not a spam email", false);
+		String result = classifier.predict(newMail);
+		assertEquals("LaPlace : ham", result);
+	}
 	
-	 @Test
-	 public void testPredictOneHamMail() {
-		 NaiveBayesClassifier classifier = new NaiveBayesClassifier();
-	     List<Mail> trainingData = Arrays.asList(
-	             new Mail("This is a spam email", true),
-	             new Mail("This is not a spam email", false)
-	     );
-	     classifier.train(trainingData);
-	     Mail newMail = new Mail("This is not a spam email", false);
-	     String result = classifier.predict(newMail);
-	     assertEquals("LaPlace : ham", result);
-	 }
-	
+	@Test
+	public void testPredictNoMail() {
+		NaiveBayesClassifier classifier = new NaiveBayesClassifier();
+		List<Mail> trainingData = Arrays.asList(
+				new Mail("This is a spam email", true)
+				);
+		classifier.train(trainingData);
+		Mail newMail = new Mail("", true);
+		String result = classifier.predict(newMail);
+		assertEquals("Empty mail content", result);
+	}
+
 }
